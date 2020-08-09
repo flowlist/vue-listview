a98949c2605eef85683347f58d93a68c53b3ffcb
falstack <icesilt@outlook.com>
2020-08-09 12:24:24 +0800

    simplfy code

--- example/plugins/prototypes.all.js
+++ example/plugins/prototypes.all.js
@@ -1,7 +1,4 @@
 import Vue from 'vue'
-import VSwitcher from 'v-switcher'
 import FlowLoader from '../../src/loader'
-import 'v-switcher/dist/v-switcher.css'
 
 Vue.component(FlowLoader.name, FlowLoader)
-Vue.component(VSwitcher.name, VSwitcher)
