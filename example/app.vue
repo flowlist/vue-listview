de8f2b600ea177365f7200ad83950a245b032003
falstack <icesilt@outlook.com>
2020-08-09 11:00:04 +0800

    update: doc

--- example/app.vue
+++ example/app.vue
@@ -13,21 +13,47 @@ ul {
   margin: 0;
 }
 
-li {
-  display: block;
-  height: 100px;
-  line-height: 100px;
-  padding-left: 50px;
-  margin: 15px 0;
-  border-radius: 5px;
+a {
+  text-decoration: none;
 }
 
 #app {
+  > header {
+    a {
+      display: block;
+      height: 40px;
+      line-height: 40px;
+      padding: 0 20px;
+      background-color: #f7f8fa;
+      color: #323233;
+      transition: background-color 0.3s;
+      font-size: 14px;
+      border-radius: 20px;
+
+      &:hover {
+        background-color: #eef0f4;
+      }
+    }
+
+    li {
+      margin-left: 15px;
+    }
+  }
+
   > section {
     width: 600px;
     margin: 0 auto;
     max-width: 100%;
     padding: 0 15px;
+
+    li {
+      display: block;
+      height: 100px;
+      line-height: 100px;
+      padding-left: 50px;
+      margin: 15px 0;
+      border-radius: 5px;
+    }
   }
 }
 
@@ -49,7 +75,21 @@ li {
 
 <template>
   <div id="app">
-    <header />
+    <header>
+      <VSwitcher
+        :headers="headers"
+        :routable="true"
+        align="start"
+      >
+        <RouterLink
+          v-for="(item, index) in headers"
+          :key="index"
+          :slot="`tab-${index}`"
+          :to="item.route"
+          v-text="item.name"
+        />
+      </VSwitcher>
+    </header>
     <section>
       <RouterView />
     </section>
@@ -64,7 +104,60 @@ export default {
   data() {
     return {}
   },
-  computed: {},
+  computed: {
+    headers() {
+      return [
+        {
+          name: '首页',
+          route: '/'
+        },
+        {
+          name: '页码翻页',
+          route: '/page'
+        },
+        {
+          name: '对象数组',
+          route: '/object'
+        },
+        {
+          name: '跳转翻页',
+          route: '/jump'
+        },
+        {
+          name: 'seenIds翻页',
+          route: '/seen_ids'
+        },
+        {
+          name: 'sinceId翻页',
+          route: '/since_id'
+        },
+        {
+          name: 'last_id翻页',
+          route: '/last_id'
+        },
+        {
+          name: '模拟异常',
+          route: '/error'
+        },
+        {
+          name: '首屏异常',
+          route: '/first-error'
+        },
+        {
+          name: '首屏加载',
+          route: '/first-loading'
+        },
+        {
+          name: '首屏为空',
+          route: '/nothing'
+        },
+        {
+          name: '动态列表',
+          route: '/comment'
+        }
+      ]
+    }
+  },
   watch: {},
   created() {},
   mounted() {},
--- example/main.js
+++ example/main.js
@@ -3,9 +3,12 @@ import App from './app'
 import store from './store'
 import router from './router'
 import FlowLoader from '../src/loader'
+import VSwitcher from 'v-switcher'
+import 'v-switcher/dist/v-switcher.css'
 
 Vue.config.productionTip = false
 Vue.component(FlowLoader.name, FlowLoader)
+Vue.component(VSwitcher.name, VSwitcher)
 
 new Vue({
   store,
